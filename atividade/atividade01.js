8ea2d3555092b3229deec2abd20ccc4f28e337ee
Renan <j.renan.r@gmail.com>
2022-02-10 01:17:20 -0300

    adicionado as atividades 1 e 2

---
+++ atividade/atividade01.js
@@ -0,0 +1,16 @@
+function verificaPalindromo(string)
+{
+    if (!string) return "string inexistente";
+
+    for(let i = 0; i < string.length; i++)
+    {
+        if(string[i] !== string[string.length - 1 - i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+} 
+
+console.log(verificaPalindromo("ovo"));
